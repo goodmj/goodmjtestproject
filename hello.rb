5a0884a09cb9bd9242774546dab3ee47da2a4894
goodmj <matthew.j.good@gmail.com>
2009-05-10 18:14:54 -0700

    Added a tiny line about why I'm playing with this project again.

--- hello.rb
+++ hello.rb
@@ -1,6 +1,6 @@
 puts "hello my name is test file #2!!!"
 
-
+puts "an update to this - tryign to get my 'home' computer properly set up with git... I have some work accounts, but its all a bit mungy."
 
 puts "this is an attempt to create a new branch, 'experiment'"
 
